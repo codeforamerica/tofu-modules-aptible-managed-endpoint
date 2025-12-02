3fe5795502eb995bbc76e3a422496d05b6a506d3
James Armes <jarmes@codeforamerica.org>
2025-12-02 13:26:05 -0500

    feat: Loosen version constraints for providers.

--- versions.tf
+++ versions.tf
@@ -3,13 +3,13 @@ terraform {
 
   required_providers {
     aptible = {
-      version = "~> 0.9"
+      version = ">= 0.9"
       source  = "aptible/aptible"
     }
 
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.44"
+      version = ">= 5.44"
     }
   }
 }
